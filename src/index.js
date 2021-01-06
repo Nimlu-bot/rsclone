6b19459779a59f3038dcd59783a0229fae851a26
Nimlu <snesterov@gmail.com>
2021-01-06 19:37:21 +0300

    fix: delete visibility of login component

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { BaseMarkup, GameField, Header, Login } from './components/index';
 const markup = new BaseMarkup();
 const gameField = new GameField();
 const header = new Header();
-const login = new Login();
+// const login = new Login();
 
 markup.init();
 gameField.init();
 header.init();
-login.init();
+// login.init();
