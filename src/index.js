bde467f21d7dded44617c743c4fd492f282a438a
ANDREYKOMOV <komov.te@gmail.com>
2021-01-07 17:41:32 +0300

    feat: add files

--- src/index.js
+++ src/index.js
@@ -10,10 +10,5 @@ markup.init();
 gameField.init();
 header.init();
 audioProcessor.init();
-setTimeout(() => {
-  audioProcessor.bark.play();
-}, 5000);
-setTimeout(() => {
-  audioProcessor.stopSound('bark');
-}, 10000);
+
 
