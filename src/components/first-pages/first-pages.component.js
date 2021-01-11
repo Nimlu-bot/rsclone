928363b15bb6cc479b7573283be2d754054e5328
ANDREYKOMOV <komov.te@gmail.com>
2021-01-11 23:15:11 +0300

    init: add audio-processor component

--- src/components/first-pages/first-pages.component.js
+++ src/components/first-pages/first-pages.component.js
@@ -1,20 +1,23 @@
-import { backBtnHeader,continueBtn } from "./psges-list/backBtnHeader/backBtnHeader.template";
+import { backBtnHeader, continueBtn } from "./psges-list/backBtnHeader/backBtnHeader.template";
 import { statisticPagesTemplate } from "./psges-list/statistic/statistic-pages.template";
 import { settingsPagesTemplate } from "./psges-list/settings/settings-pages.template";
 import { navPagesTemplate } from "./psges-list/nav/nav-pages.template";
 import { levelPagesTemplate } from "./psges-list/level/level-pages.template";
 import { firstPagesTemplate } from "./first-pages.template";
 // Tonya
-import {GameField} from "../game-field/game-field.component"
+import { GameField } from "../game-field/game-field.component"
 // Sergey
 import { Login } from '../login/login.component';
+// Andrey
+import AudioProcessor from "../audio-processor/audio-processor.component";
+
 
 export class FirstPages {
     constructor() {
         this.title = "Game menu";
     }
 
-    nav(){
+    nav() {
         this.title = "Game menu"
         document.querySelector(".pages").innerHTML = this.title;
         document.querySelector(".game-menu").insertAdjacentHTML("afterbegin", navPagesTemplate);
@@ -22,32 +25,32 @@ export class FirstPages {
         // eslint-disable-next-line consistent-return
         document.querySelector(".nav").addEventListener('click', (e) => {
 
-            if(!(document.querySelector(".login-wrapper")) === false ) {
+            if (!(document.querySelector(".login-wrapper")) === false) {
                 document.querySelector(".login-wrapper").remove()
             }
-                
+
             // eslint-disable-next-line prefer-const
             switch (e.target.id) {
-                
-                case 'start': 
+
+                case 'start':
                     document.querySelector(".nav").remove()
                     return this.start()
-              
-                case 'level': 
+
+                case 'level':
                     document.querySelector(".nav").remove()
                     return this.level()
-        
-                case 'settings': 
+
+                case 'settings':
                     document.querySelector(".nav").remove()
                     return this.settings()
-       
-                case 'statistic': 
+
+                case 'statistic':
                     document.querySelector(".nav").remove()
                     return this.statistic()
-            
+
                 default:
-                
-                break
+
+                    break
             }
         })
     }
@@ -56,16 +59,16 @@ export class FirstPages {
         this.nav()
         document.querySelector(".game-menu").style.backgroundColor = "rgba(50, 150, 141, 0.9)";
         document.querySelector(".nav").insertAdjacentHTML('afterbegin', continueBtn)
-        document.querySelector(".continue-btn").addEventListener("click",() => {
+        document.querySelector(".continue-btn").addEventListener("click", () => {
             document.querySelector(".nav").remove();
             document.querySelector(".game-menu").style.backgroundColor = "transparent";
             this.pauseBtn()
         })
     }
 
-    pauseBtn(){
+    pauseBtn() {
         document.querySelector(".pause-btn").insertAdjacentHTML("afterbegin", backBtnHeader);
-        document.querySelector(".pause-btn-header").addEventListener("click",()=>{
+        document.querySelector(".pause-btn-header").addEventListener("click", () => {
             document.querySelector(".pause-btn-header").remove();
             this.continue();
         })
@@ -87,10 +90,10 @@ export class FirstPages {
         document.querySelector(".game-menu").insertAdjacentHTML("afterbegin", levelPagesTemplate);
         // eslint-disable-next-line consistent-return
         document.querySelector(".level").addEventListener('click', (e) => {
-            if(e.target.id === 'level-back'){
+            if (e.target.id === 'level-back') {
                 document.querySelector(".level").remove();
                 return this.nav()
-            // eslint-disable-next-line no-else-return
+                // eslint-disable-next-line no-else-return
             } else {
                 document.querySelector(".level").remove()
                 this.start()
@@ -98,31 +101,53 @@ export class FirstPages {
         })
     }
 
-    settings(){
+    settings() {
         this.title = "settings"
         document.querySelector(".pages").innerHTML = this.title;
         document.querySelector(".game-menu").insertAdjacentHTML("afterbegin", settingsPagesTemplate);
         // eslint-disable-next-line consistent-return
         document.querySelector(".settings").addEventListener('click', (e) => {
-            if(e.target.id === 'settings-back'){
+            if (e.target.id === 'settings-back') {
                 document.querySelector(".settings").remove();
                 return this.nav()
             }
-        })
+        });
+
+        this.volumeChanger();
+        this.panChanger();
+
     }
 
-    statistic(){
+    statistic() {
         this.title = "statistic"
         document.querySelector(".pages").innerHTML = this.title;
         document.querySelector(".game-menu").insertAdjacentHTML("afterbegin", statisticPagesTemplate);
         // eslint-disable-next-line consistent-return
         document.querySelector(".statistic").addEventListener('click', (e) => {
-            if(e.target.id === 'statistic-back'){
+            if (e.target.id === 'statistic-back') {
                 document.querySelector(".statistic").remove();
                 return this.nav()
             }
         })
-        
+
+    }
+
+    volumeChanger() {
+        // volume changer
+        const volumeChanger = document.querySelector('#volume');
+        volumeChanger.value = AudioProcessor.gain;
+        volumeChanger.addEventListener('input', (event) => {
+            AudioProcessor.setVolume(event.target.value);
+        }, false);
+    }
+
+    panChanger() {
+        // panner (left/right speacker)
+        const panChanger = document.querySelector('#panner');
+        panChanger.value = AudioProcessor.pan;
+        panChanger.addEventListener('input', (event) => {
+            AudioProcessor.setPan(event.target.value);
+        }, false);
     }
 
     init() {
