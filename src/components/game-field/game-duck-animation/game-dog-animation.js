6e8d2dc425ca6694a442be42e7fd6466da840ed6
tone4ka <toniamik@gmail.com>
2021-01-13 10:19:39 +0300

    feat: animetion correction

--- src/components/game-field/game-duck-animation/game-dog-animation.js
+++ src/components/game-field/game-duck-animation/game-dog-animation.js
@@ -32,10 +32,11 @@ export function dogMove(ctx,time,ducksMove){
         if(frameCounter<35){// иначе замирает и принюхивается
             dogGoX+=5;
             frameNum+=1;
+            if(frameNum>4)frameNum=0;
+        }else{
+            frameNum===3 ? frameNum=4 : frameNum=3;
         }
-        if(frameNum>4)frameNum=0;
         frameCounter+=1;
- 
         if(frameCounter===45){
             dog.go=false;
             dog.jump=true;
@@ -52,16 +53,15 @@ export function dogMove(ctx,time,ducksMove){
         } else{
             ctx.globalCompositeOperation = 'source-over';
             frameString=1;
-            ctx.drawImage(dogImg, 560*frameNum, 410*frameString, 560, 600, dogGoX,  dogGoY-20, 264, 270);
+            if(frameCounter<7){
+                ctx.drawImage(dogImg, 560*frameNum, 410*frameString, 560, 600, dogGoX,  dogGoY-20, 264, 270);
+            }
             frameCounter+=1;
             if(frameCounter>2){// замерла с поднятыми ушами
                 if(frameNum<2)frameNum+=1;
-                dogGoX+=8;
+                dogGoX+=10;
                 dogGoY-=10;
-            
-            
-            // if(frameNum===3){
-                if(frameCounter>7){
+                if(frameCounter>40){// прыгнула
                 console.log('i am jump');
                 dog.jump=false;
                 dog.scaredDucks=true;
@@ -78,40 +78,55 @@ export function dogMove(ctx,time,ducksMove){
     }
     if(dog.findOneDuck){
         clearInterval(time.moveIntervalId);
-        time.frameTime=20;
+        time.frameTime=40;
         time.moveIntervalId=setInterval(()=>ducksMove(/* level */),time.frameTime);
         frameString=0;
         frameNum=5
-        ctx.drawImage(dogImg, 559*frameNum, 402*frameString, 520, 402, dogIngrassX,  dogInGrassY, 220, 190);
-        dogInGrassY-=1;
+        if(frameCounter<40){
+            ctx.drawImage(dogImg, 559*frameNum, 402*frameString, 520, 402, dogIngrassX,  dogInGrassY, 220, 190);
+        }
+        if(frameCounter<12){
+            dogInGrassY-=3;
+        }else{
+            dogInGrassY+=2;          
+        }
+        // dogInGrassY-=1;
         frameCounter+=1;
-        if(frameCounter===40){
+        if(frameCounter===120){
             dog.findOneDuck=false;
             dogIngrassX=290;
             dogInGrassY=370;
             frameCounter=0;
             frameString=0;
             frameNum=0;
-            dog.go=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
+            dog.scaredDucks=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
+            // dog.go=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
         }
     }
     if(dog.findTwoDucks){
         clearInterval(time.moveIntervalId);
-        time.frameTime=20;
+        time.frameTime=30;
         time.moveIntervalId=setInterval(()=>ducksMove(/* level */),time.frameTime);
         frameString=1;
         frameNum=5;
-        ctx.drawImage(dogImg, 540*frameNum, 430*frameString, 600, 590, dogIngrassX,  dogInGrassY, 250, 230);
-        dogInGrassY-=1;
-        frameCounter+=1;
-        if(frameCounter===50){
+        if(frameCounter<50){
+            ctx.drawImage(dogImg, 540*frameNum, 430*frameString, 600, 590, dogIngrassX,  dogInGrassY, 250, 230);
+        }
+        if(frameCounter<20){
+            dogInGrassY-=3;
+        }else{
+            dogInGrassY+=2;          
+        }
+        frameCounter+=1; 
+        if(frameCounter===120){
             dog.findTwoDucks=false;
             dogIngrassX=290;
             dogInGrassY=370;
             frameCounter=0;
             frameString=0;
             frameNum=0;
-            dog.go=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
+            dog.scaredDucks=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
+            // dog.go=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
         }
     }
     if(dog.laught){
@@ -119,8 +134,14 @@ export function dogMove(ctx,time,ducksMove){
         time.frameTime=100;
         time.moveIntervalId=setInterval(()=>ducksMove(/* level */),time.frameTime);
         frameString=1;
-        ctx.drawImage(dogImg, 540*frameNumLaught, 430*frameString, 500, 590, dogIngrassX,  dogInGrassY-50, 220, 230);
-        dogInGrassY===370 ? dogInGrassY+=10 : dogInGrassY=370;
+        if(frameCounterLaught<20){
+            ctx.drawImage(dogImg, 540*frameNumLaught, 430*frameString, 500, 590, dogIngrassX,  dogInGrassY-50, 220, 230);
+                if(frameCounterLaught<15){
+                    dogInGrassY===370 ? dogInGrassY+=10 : dogInGrassY=370;
+                }else{
+                    dogInGrassY+=10;
+                }
+        }
         frameNumLaught===3 ? frameNumLaught = 4 : frameNumLaught = 3;
         frameCounterLaught+=1;
         if(frameCounterLaught===50){
@@ -128,7 +149,8 @@ export function dogMove(ctx,time,ducksMove){
             dogInGrassY=370;
             frameString=0;
             frameCounterLaught=0;
-            dog.go=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
+            dog.scaredDucks=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
+            // dog.go=true;// условие продолжения игры!!!!!!!!!!!!!!!!!!!!???????????????????????
         }
 
     }
