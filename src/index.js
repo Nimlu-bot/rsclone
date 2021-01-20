7ec3e5bed944c5e561bf04541b0f084f515a14f6
ANDREYKOMOV <komov.te@gmail.com>
2021-01-20 19:20:21 +0300

    feat: add changes to main index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-// fedya
 import { FirstPages } from "./components/index";
 import { SvgAnimation } from "./components/svg-animation/index";
 
 const firstPages = new FirstPages();
-// firstPages.init();
 
-// ---
 const svgAnimation = new SvgAnimation();
 svgAnimation.makeGood();
 svgAnimation.getButton().addEventListener('click', () => {
   svgAnimation.removeAll();
   firstPages.init();
-})
+});
