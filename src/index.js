38e4bd90af960b0426391bdc93b276fcd3f4c0e9
ANDREYKOMOV <komov.te@gmail.com>
2021-01-21 00:53:57 +0300

    feat: edit animation and modal windows colors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { FirstPages } from "./components/index";
 import { SvgAnimation } from "./components/svg-animation/index";
+import { ModalWindow } from "./components/modal-window/index";
 
 const firstPages = new FirstPages();
+
+new ModalWindow('perfect').createWindow();
+new ModalWindow('game-over').createWindow();
 
 const svgAnimation = new SvgAnimation();
 svgAnimation.makeGood();
