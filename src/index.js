c96a8684c50ac4926ef31808c39849fc18e7d750
ANDREYKOMOV <komov.te@gmail.com>
2021-01-21 15:50:13 +0300

    init: add fix audio in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { FirstPages } from "./components/index";
 import { SvgAnimation } from "./components/svg-animation/index";
 import { ModalWindow } from "./components/modal-window/index";
+import AudioProcessor from "./components/audio-processor/audio-processor.component";
 
 const firstPages = new FirstPages();
 
@@ -11,5 +12,7 @@ const svgAnimation = new SvgAnimation();
 svgAnimation.makeGood();
 svgAnimation.getButton().addEventListener('click', () => {
   svgAnimation.removeAll();
+  AudioProcessor.init();
+  AudioProcessor.play('quack');
   firstPages.init();
 });
