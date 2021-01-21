2bbba2ab451c86a377308b787108272069ffe35e
tone4ka <toniamik@gmail.com>
2021-01-21 10:50:38 +0300

    fix: statistic correction merge dev need

--- src/components/game-field/game-duck-animation/game-dog-animation.js
+++ src/components/game-field/game-duck-animation/game-dog-animation.js
@@ -1,189 +1,196 @@
 /* eslint-disable no-param-reassign */
 import AudioProcessor from "../../audio-processor/audio-processor.component";
-import {incDuckCount} from "../../../core/user-statistic"
 
-const dogImg=document.createElement('img');
-dogImg.src='../../../assets/img/pes.png';
-let frameNum=0;
-let frameString=0;
-let frameNumLaught=3;
-let dogGoX=0;
-let dogGoY=400;
-let dogIngrassX=290;
-let dogInGrassY=370;
-let frameCounter=0;
-let frameCounterLaught=0;
+const dogImg = document.createElement("img");
+dogImg.src = "../../../assets/img/pes.png";
+let frameNum = 0;
+let frameString = 0;
+let frameNumLaught = 3;
+let dogGoX = 0;
+let dogGoY = 400;
+let dogIngrassX = 290;
+let dogInGrassY = 370;
+let frameCounter = 0;
+let frameCounterLaught = 0;
 
-export const dog={
-    go:true,
-    jump:false,
-    findOneDuck:false,
-    findTwoDucks:false,
-    laught:false,
-    scaredDucks:false
+export const dog = {
+    go: true,
+    jump: false,
+    findOneDuck: false,
+    findTwoDucks: false,
+    laught: false,
+    scaredDucks: false
+};
+
+export function newDogParameters() {
+    frameNum = 0;
+    frameString = 0;
+    frameNumLaught = 3;
+    dogGoX = 0;
+    dogGoY = 400;
+    dogIngrassX = 290;
+    dogInGrassY = 370;
+    frameCounter = 0;
+    frameCounterLaught = 0;
+    dog.go = true;
+    dog.jump = false;
+    dog.findOneDuck = false;
+    dog.findTwoDucks = false;
+    dog.laught = false;
+    dog.scaredDucks = false;
 }
 
-export function newDogParameters(){
-    frameNum=0;
-    frameString=0;
-    frameNumLaught=3;
-    dogGoX=0;
-    dogGoY=400;
-    dogIngrassX=290;
-    dogInGrassY=370;
-    frameCounter=0;
-    frameCounterLaught=0;
-    dog.go=true;
-    dog.jump=false;
-    dog.findOneDuck=false;
-    dog.findTwoDucks=false;
-    dog.laught=false;
-    dog.scaredDucks=false;
-}
-
-export function dogMove(ctx,time,gameProcess, progress, showCurrentStatistic){
+export function dogMove(ctx, time, gameProcess, progress, showCurrentStatistic) {
     clearInterval(time.moveIntervalId);
-    time.frameTime=100;
-    time.moveIntervalId=setInterval(()=>gameProcess(/* level */),time.frameTime);
-    if(dog.go){
-        dogGoY=410
-        ctx.globalCompositeOperation = 'source-over';
-        ctx.drawImage(dogImg, 560*frameNum, 390*frameString, 555, 410, dogGoX,  dogGoY, 250, 180);
-        if(frameCounter<35){// иначе замирает и принюхивается
-            dogGoX+=5;
-            frameNum+=1;
-            if(frameNum>4)frameNum=0;
-        }else{
-            frameNum===3 ? frameNum=4 : frameNum=3;
+    time.frameTime = 100;
+    time.moveIntervalId = setInterval(() => gameProcess(/* level */), time.frameTime);
+    if (dog.go) {
+        dogGoY = 410;
+        ctx.globalCompositeOperation = "source-over";
+        ctx.drawImage(dogImg, 560 * frameNum, 390 * frameString, 555, 410, dogGoX, dogGoY, 250, 180);
+        if (frameCounter < 35) {
+            // иначе замирает и принюхивается
+            dogGoX += 5;
+            frameNum += 1;
+            if (frameNum > 4) frameNum = 0;
+        } else {
+            frameNum === 3 ? (frameNum = 4) : (frameNum = 3);
         }
-        frameCounter+=1;
-        if(frameCounter===45){
-            dog.go=false;
-            dog.jump=true;
-            frameCounter=0;
-            frameNum=0;
-            dogGoY=350;
+        frameCounter += 1;
+        if (frameCounter === 45) {
+            dog.go = false;
+            dog.jump = true;
+            frameCounter = 0;
+            frameNum = 0;
+            dogGoY = 350;
         }
     }
 
-    if(dog.jump){
-        if(frameCounter===0){
-            frameCounter+=1;
-        } else{
-            ctx.globalCompositeOperation = 'source-over';
-            frameString=1;
-            if(frameCounter<7){
-                ctx.drawImage(dogImg, 560*frameNum, 410*frameString, 560, 600, dogGoX,  dogGoY-20, 264, 270);
+    if (dog.jump) {
+        if (frameCounter === 0) {
+            frameCounter += 1;
+        } else {
+            ctx.globalCompositeOperation = "source-over";
+            frameString = 1;
+            if (frameCounter < 7) {
+                ctx.drawImage(dogImg, 560 * frameNum, 410 * frameString, 560, 600, dogGoX, dogGoY - 20, 264, 270);
             }
-            frameCounter+=1;
+            frameCounter += 1;
             // soundFlag=true;
-            if(frameCounter>2){// замерла с поднятыми ушами
-                if(frameNum<2)frameNum+=1;
-                dogGoX+=10;
-                dogGoY-=10;
-                if(frameCounter>30){// прыгнула
-                    AudioProcessor.play('bark');
-                    dog.jump=false;
-                    dog.scaredDucks=true;
-                    console.log('ducks scared');
-                    incDuckCount();// ! статистика
-                    frameNum=0;
-                    frameString=0;
-                    dogGoX=0;
-                    dogGoY=400;
-                    frameCounter=0;
+            if (frameCounter > 2) {
+                // замерла с поднятыми ушами
+                if (frameNum < 2) frameNum += 1;
+                dogGoX += 10;
+                dogGoY -= 10;
+                if (frameCounter > 30) {
+                    // прыгнула
+                    AudioProcessor.play("bark");
+                    dog.jump = false;
+                    dog.scaredDucks = true;
+                    console.log("ducks scared");
+                    frameNum = 0;
+                    frameString = 0;
+                    dogGoX = 0;
+                    dogGoY = 400;
+                    frameCounter = 0;
                 }
             }
         }
-
     }
-    if(dog.findOneDuck){
+    if (dog.findOneDuck) {
         clearInterval(time.moveIntervalId);
-        progress.bullet=0;
+        progress.bullet = 0;
         showCurrentStatistic(progress);
-        time.frameTime=40;
-        time.moveIntervalId=setInterval(()=>gameProcess(/* level */),time.frameTime);
-        frameString=0;
-        frameNum=5
-        if(frameCounter<40){
-            ctx.drawImage(dogImg, 559*frameNum, 402*frameString, 520, 402, dogIngrassX,  dogInGrassY, 220, 190);
+        time.frameTime = 40;
+        time.moveIntervalId = setInterval(() => gameProcess(/* level */), time.frameTime);
+        frameString = 0;
+        frameNum = 5;
+        if (frameCounter < 40) {
+            ctx.drawImage(dogImg, 559 * frameNum, 402 * frameString, 520, 402, dogIngrassX, dogInGrassY, 220, 190);
         }
-        if(frameCounter<12){
-            dogInGrassY-=3;
-        }else{
-            dogInGrassY+=2;          
+        if (frameCounter < 12) {
+            dogInGrassY -= 3;
+        } else {
+            dogInGrassY += 2;
         }
         // dogInGrassY-=1;
-        frameCounter+=1;
-        if(frameCounter===120){
-            dog.findOneDuck=false;
-            dogIngrassX=290;
-            dogInGrassY=370;
-            frameCounter=0;
-            frameString=0;
-            frameNum=0;
-            progress.bullet=4;
+        frameCounter += 1;
+        if (frameCounter === 120) {
+            dog.findOneDuck = false;
+            dogIngrassX = 290;
+            dogInGrassY = 370;
+            frameCounter = 0;
+            frameString = 0;
+            frameNum = 0;
+            progress.bullet = 4;
             showCurrentStatistic(progress);
-            dog.scaredDucks=true;
+            dog.scaredDucks = true;
         }
     }
-    if(dog.findTwoDucks){
+    if (dog.findTwoDucks) {
         clearInterval(time.moveIntervalId);
-        progress.bullet=0;
+        progress.bullet = 0;
         showCurrentStatistic(progress);
-        time.frameTime=30;
-        time.moveIntervalId=setInterval(()=>gameProcess(/* level */),time.frameTime);
-        frameString=1;
-        frameNum=5;
-        if(frameCounter<50){
-            ctx.drawImage(dogImg, 540*frameNum, 430*frameString, 600, 590, dogIngrassX,  dogInGrassY, 250, 230);
+        time.frameTime = 30;
+        time.moveIntervalId = setInterval(() => gameProcess(/* level */), time.frameTime);
+        frameString = 1;
+        frameNum = 5;
+        if (frameCounter < 50) {
+            ctx.drawImage(dogImg, 540 * frameNum, 430 * frameString, 600, 590, dogIngrassX, dogInGrassY, 250, 230);
         }
-        if(frameCounter<20){
-            dogInGrassY-=3;
-        }else{
-            dogInGrassY+=2;          
+        if (frameCounter < 20) {
+            dogInGrassY -= 3;
+        } else {
+            dogInGrassY += 2;
         }
-        frameCounter+=1; 
-        if(frameCounter===120){
-            dog.findTwoDucks=false;
-            dogIngrassX=290;
-            dogInGrassY=370;
-            frameCounter=0;
-            frameString=0;
-            frameNum=0;
-            progress.bullet=4;
+        frameCounter += 1;
+        if (frameCounter === 120) {
+            dog.findTwoDucks = false;
+            dogIngrassX = 290;
+            dogInGrassY = 370;
+            frameCounter = 0;
+            frameString = 0;
+            frameNum = 0;
+            progress.bullet = 4;
             showCurrentStatistic(progress);
-            dog.scaredDucks=true;
+            dog.scaredDucks = true;
         }
     }
-    if(dog.laught){
+    if (dog.laught) {
         clearInterval(time.moveIntervalId);
-        progress.bullet=0;
+        progress.bullet = 0;
         showCurrentStatistic(progress);
-        time.frameTime=100;
-        time.moveIntervalId=setInterval(()=>gameProcess(/* level */),time.frameTime);
-        frameString=1;
-        if(frameCounterLaught<20){
-            ctx.drawImage(dogImg, 540*frameNumLaught, 430*frameString, 500, 590, dogIngrassX,  dogInGrassY-50, 220, 230);
-                if(frameCounterLaught<15){
-                    dogInGrassY===370 ? dogInGrassY+=10 : dogInGrassY=370;
-                }else{
-                    dogInGrassY+=10;
-                }
+        time.frameTime = 100;
+        time.moveIntervalId = setInterval(() => gameProcess(/* level */), time.frameTime);
+        frameString = 1;
+        if (frameCounterLaught < 20) {
+            ctx.drawImage(
+                dogImg,
+                540 * frameNumLaught,
+                430 * frameString,
+                500,
+                590,
+                dogIngrassX,
+                dogInGrassY - 50,
+                220,
+                230
+            );
+            if (frameCounterLaught < 15) {
+                dogInGrassY === 370 ? (dogInGrassY += 10) : (dogInGrassY = 370);
+            } else {
+                dogInGrassY += 10;
+            }
         }
-        frameNumLaught===3 ? frameNumLaught = 4 : frameNumLaught = 3;
-        frameCounterLaught+=1;
-        if(frameCounterLaught===50){
-            dog.laught=false;
-            dogInGrassY=370;
-            frameString=0;
-            frameCounterLaught=0;
-            progress.bullet=4;
+        frameNumLaught === 3 ? (frameNumLaught = 4) : (frameNumLaught = 3);
+        frameCounterLaught += 1;
+        if (frameCounterLaught === 50) {
+            dog.laught = false;
+            dogInGrassY = 370;
+            frameString = 0;
+            frameCounterLaught = 0;
+            progress.bullet = 4;
             showCurrentStatistic(progress);
-            dog.scaredDucks=true;
+            dog.scaredDucks = true;
         }
-
     }
-
-
-};
+}
--- src/components/game-field/game-duck-animation/game-duck-process.js
+++ src/components/game-field/game-duck-animation/game-duck-process.js
@@ -1,202 +1,214 @@
 /* eslint-disable no-use-before-define */
 import { CANVAS_HEIGTH, CANVAS_WIDTH } from "../../../core/index";
-import {duckMove, duckGoAway, duckShot, newDucksParameters} from './game-duck-duck-move';
-import {ducksForGame, progressForGame, newProgressParameters, startGameProgressParameters} from './game-constants';
-import {dog, dogMove, newDogParameters} from './game-dog-animation';
-import {showCurrentStatistic} from './game-show-current-statistic-function';
+import { duckMove, duckGoAway, duckShot, newDucksParameters } from "./game-duck-duck-move";
+import { ducksForGame, progressForGame, newProgressParameters, startGameProgressParameters } from "./game-constants";
+import { dog, dogMove, newDogParameters } from "./game-dog-animation";
+import { showCurrentStatistic } from "./game-show-current-statistic-function";
 import { ModalWindow } from "../../modal-window/modal-window.component";
-import {startGameStat, statStart, newRound, isBuletsEnd, isLevelEnd, isWin} from "../../../core/user-statistic"
-import {cloudsAdd} from "./game-clouds";
+import { startGameStat, statStart, newRound, isBuletsEnd, isLevelEnd, isWin } from "../../../core/user-statistic";
+import { cloudsAdd } from "./game-clouds";
 
-
-const treeGrass=document.createElement('img');
-treeGrass.src='../../../assets/img/background_full.png';
+const treeGrass = document.createElement("img");
+treeGrass.src = "../../../assets/img/background_full.png";
 let ctx;
 let canvas;
-let pauseFlag=false;
-let gameFlag=false;
-let lvlbeginStatisticFlag=true;
-const time={
-    frameTime:80,
-    moveIntervalId:null
-}
-let shotListenerFlag=false;
+let pauseFlag = false;
+let gameFlag = false;
+let lvlbeginStatisticFlag = true;
+const time = {
+    frameTime: 80,
+    moveIntervalId: null
+};
+let shotListenerFlag = false;
 const ducks = ducksForGame;
 const progress = progressForGame;
 const dogObj = dog;
 
-function continueGame(event){
-    if(event.target.classList.contains("continue-btn")||event.target.id==='to-main'){
-        if (gameFlag) startGame(null, null);// возвращаемся в игру, не меняя параметры
+function continueGame(event) {
+    if (event.target.classList.contains("continue-btn") || event.target.id === "to-main") {
+        if (gameFlag) startGame(null, null); // возвращаемся в игру, не меняя параметры
     }
 }
 
-function pauseGame(){
+function pauseGame() {
     if (gameFlag) {
-        pauseFlag=true;
+        pauseFlag = true;
         clearInterval(time.moveIntervalId);
-        document.body.addEventListener('click', (event)=>continueGame(event));
+        document.body.addEventListener("click", (event) => continueGame(event));
     }
 }
 
 // для модальных окон
-const modalWindowPerfect=new ModalWindow('perfect');
-const modalWindowGameOver=new ModalWindow('game-over');
+const modalWindowPerfect = new ModalWindow("perfect");
+const modalWindowGameOver = new ModalWindow("game-over");
 
-function showModalWindow(){
-        if(progress.shotDucks>=5){
-            modalWindowPerfect.showWindow();
-            newProgressParameters();
-            progress.level+=1;
-        }else{
-            modalWindowGameOver.showWindow();
-            startGameProgressParameters();// Обнуляем очки
-        }
-        pauseGame();
+function showModalWindow() {
+    if (progress.shotDucks >= 5) {
+        modalWindowPerfect.showWindow();
+        newProgressParameters();
+        progress.level += 1;
+    } else {
+        modalWindowGameOver.showWindow();
+        startGameProgressParameters(); // Обнуляем очки
+    }
+    pauseGame();
 }
 
-function gameProcess(){
-    if(lvlbeginStatisticFlag)statStart();// ! статистика
-    lvlbeginStatisticFlag=false;// ! статистика
-    gameFlag=true;
-    pauseFlag=false;
+function gameProcess() {
+    if (lvlbeginStatisticFlag) startGameStat(); // ! статистика
+    lvlbeginStatisticFlag = false; // ! статистика
+    gameFlag = true;
+    pauseFlag = false;
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGTH);
     // отрисовываем фон
     ctx.drawImage(treeGrass, 0, 0, 1008, 724, 0, 80, CANVAS_WIDTH, CANVAS_HEIGTH);
-    dogMove(ctx,time,gameProcess, progress, showCurrentStatistic);// !!!!!!!!!!!!!!!!!!!!!!!собачка
+    dogMove(ctx, time, gameProcess, progress, showCurrentStatistic); // !!!!!!!!!!!!!!!!!!!!!!!собачка
 
-    if(dogObj.scaredDucks) {
+    if (dogObj.scaredDucks) {
         // ускоряем движение
         clearInterval(time.moveIntervalId);
-        time.frameTime=85-progress.level*7;
-        time.moveIntervalId=setInterval(()=>gameProcess(/* level */),time.frameTime);
-        ctx.globalCompositeOperation = 'destination-over';
-        if((ducks.duck1.timeAfterStartFly<Math.ceil(200*(80/time.frameTime))) && (progress.bullet!==0)) {
-            if(ducks.duck1.isLive){
-                    duckMove(ctx, ducks.duck1, ducks);
-                    ducks.duck1.timeAfterStartFly+=1;
+        time.frameTime = 85 - progress.level * 7;
+        time.moveIntervalId = setInterval(() => gameProcess(/* level */), time.frameTime);
+        ctx.globalCompositeOperation = "destination-over";
+        if (ducks.duck1.timeAfterStartFly < Math.ceil(200 * (80 / time.frameTime)) && progress.bullet !== 0) {
+            if (ducks.duck1.isLive) {
+                duckMove(ctx, ducks.duck1, ducks);
+                ducks.duck1.timeAfterStartFly += 1;
             } else {
-                    duckShot(ducks.duck1, ctx, progress);
+                duckShot(ducks.duck1, ctx, progress);
             }
-        }else if(ducks.duck1.isLive){
+        } else if (ducks.duck1.isLive) {
             duckGoAway(ducks.duck1, ctx, progress);
         }
-        if((ducks.duck2.timeAfterStartFly<Math.ceil(200*(80/time.frameTime))) && (progress.bullet!==0) ) {// если еще не закончилось время и пули
-            if(ducks.duck2.isLive){
+        if (ducks.duck2.timeAfterStartFly < Math.ceil(200 * (80 / time.frameTime)) && progress.bullet !== 0) {
+            // если еще не закончилось время и пули
+            if (ducks.duck2.isLive) {
                 duckMove(ctx, ducks.duck2, ducks);
-                ducks.duck2.timeAfterStartFly+=1;
-            } else{
+                ducks.duck2.timeAfterStartFly += 1;
+            } else {
                 duckShot(ducks.duck2, ctx, progress);
             }
-        }else if(ducks.duck2.isLive){
+        } else if (ducks.duck2.isLive) {
             duckGoAway(ducks.duck2, ctx, progress);
         }
-        if(progress.currentTwoDucksCruck===2){// выбыла пара уток
-            newRound();// ! статистика
-            if(progress.currentTwoShotDucks===1)dogObj.findOneDuck=true;// если поймали одну утку
-            if(progress.currentTwoShotDucks===2)dogObj.findTwoDucks=true;// если поймали две утки
-            if(progress.currentTwoShotDucks===0)dogObj.laught=true;// если не поймали ни одной утки
-            progress.currentTwoDucksCruck=0;
-            progress.currentTwoShotDucks=0;
+        if (progress.currentTwoDucksCruck === 2) {
+            // выбыла пара уток
+            newRound(); // ! статистика
+            if (progress.currentTwoShotDucks === 1) dogObj.findOneDuck = true; // если поймали одну утку
+            if (progress.currentTwoShotDucks === 2) dogObj.findTwoDucks = true; // если поймали две утки
+            if (progress.currentTwoShotDucks === 0) dogObj.laught = true; // если не поймали ни одной утки
+            progress.currentTwoDucksCruck = 0;
+            progress.currentTwoShotDucks = 0;
             // progress.bullet=4;
             newDucksParameters(ducks);
-            dogObj.scaredDucks=false;
+            dogObj.scaredDucks = false;
             showCurrentStatistic(progress);
         }
-        if(progress.cruckDuck===10){// конец уровня
-            isLevelEnd(); // ! статистика
-            if(isLevelEnd()){isWin()}; // ! статистика
-            if (progress.level<10){
-                showCurrentStatistic(progress); 
+        if (progress.cruckDuck === 10) {
+            // конец уровня
+            if (isLevelEnd()) {
+                isWin();
+                statStart();
+            } // ! статистика
+            if (progress.level < 10) {
+                showCurrentStatistic(progress);
                 showModalWindow();
                 // progress.level+=1;
                 newDogParameters(); // для выхода собаки между уровнями
-            }else{// конец игры
+            } else {
+                // конец игры
                 showCurrentStatistic(progress);
                 showModalWindow();
                 newDogParameters(); // для выхода собаки между уровнями
-                dogObj.go=false;
-                console.log('ALL LEVELS COMPLETE');
+                dogObj.go = false;
+                console.log("ALL LEVELS COMPLETE");
                 clearInterval(time.moveIntervalId);
             }
         }
     }
-    cloudsAdd(ctx,progress.level);
+    cloudsAdd(ctx, progress.level);
+    // cloudsAdd(ctx, 9);
 }
 
-function shot(event){
+function shot(event) {
     // увеличиваем радиус попадания при увеличении скорости
-    let hittingError=0;
+    let hittingError = 0;
     switch (progress.level) {
         case 6:
-            hittingError=5;
+            hittingError = 5;
             break;
         case 7:
-            hittingError=10;
+            hittingError = 10;
             break;
         case 8:
-            hittingError=15;
+            hittingError = 15;
             break;
         case 9:
-            hittingError=18;
+            hittingError = 18;
             break;
         case 10:
-            hittingError=20;
+            hittingError = 20;
             break;
         default:
             break;
     }
-    if(gameFlag && !pauseFlag && progress.bullet>0) progress.bullet-=1;
+    if (gameFlag && !pauseFlag && progress.bullet > 0) progress.bullet -= 1;
     isBuletsEnd(); // ! статистика
-    if(progress.bullet>0){
-            if(!pauseFlag){
-                const clickX = event.clientX - canvas.getBoundingClientRect().left+25;
-                const clickY = event.clientY - canvas.getBoundingClientRect().top+25;
-                if(clickY<480){
-                    if((clickX > (ducks.duck1.moveX +5) && clickX < (ducks.duck1.moveX + 101-5+hittingError)) 
-                    && ((clickY > (ducks.duck1.moveY +5) && clickY < (ducks.duck1.moveY + 90 -5+hittingError)))){
-                        ducks.duck1.isLive=false;
-                        }
-                    if((clickX > (ducks.duck2.moveX +5) && clickX < (ducks.duck2.moveX + 101-5+hittingError)) 
-                    && ((clickY > (ducks.duck2.moveY +5) && clickY < (ducks.duck2.moveY + 90 -5+hittingError)))){
-                        ducks.duck2.isLive=false;
-                        }
-                    }
+    if (progress.bullet > 0) {
+        if (!pauseFlag) {
+            const clickX = event.clientX - canvas.getBoundingClientRect().left + 25;
+            const clickY = event.clientY - canvas.getBoundingClientRect().top + 25;
+            if (clickY < 480) {
+                if (
+                    clickX > ducks.duck1.moveX + 5 &&
+                    clickX < ducks.duck1.moveX + 101 - 5 + hittingError &&
+                    clickY > ducks.duck1.moveY + 5 &&
+                    clickY < ducks.duck1.moveY + 90 - 5 + hittingError
+                ) {
+                    ducks.duck1.isLive = false;
+                }
+                if (
+                    clickX > ducks.duck2.moveX + 5 &&
+                    clickX < ducks.duck2.moveX + 101 - 5 + hittingError &&
+                    clickY > ducks.duck2.moveY + 5 &&
+                    clickY < ducks.duck2.moveY + 90 - 5 + hittingError
+                ) {
+                    ducks.duck2.isLive = false;
                 }
             }
-            showCurrentStatistic(progress);
+        }
+    }
+    showCurrentStatistic(progress);
 }
 
-
-export function startGame (context, lvl){ // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!export
-    canvas = document.querySelector('.game-canvas');
+export function startGame(context, lvl) {
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!export
+    canvas = document.querySelector(".game-canvas");
     clearInterval(time.moveIntervalId);
     showCurrentStatistic(progress);
 
-    if(context){// запуск начала игры(при продолжении взамен контекста ставлю null)
+    if (context) {
+        // запуск начала игры(при продолжении взамен контекста ставлю null)
         startGameStat(); // ! статистика
         startGameProgressParameters();
-        if(lvl) progress.level=lvl;
+        if (lvl) progress.level = lvl;
         newDogParameters();
         newDucksParameters(ducks);
-        progress.ducksInCurrentLvl+=2;
-        ctx=context;
+        progress.ducksInCurrentLvl += 2;
+        ctx = context;
     }
-    
-    if(!pauseFlag){
+
+    if (!pauseFlag) {
         newDucksParameters(ducks);
-        progress.ducksInCurrentLvl+=2;
+        progress.ducksInCurrentLvl += 2;
     }
-    
-    const pauseBtn =document.querySelector('.pause-btn-header');
-    if(pauseBtn)pauseBtn.addEventListener('click', ()=>pauseGame());
 
-    time.moveIntervalId= setInterval(()=>gameProcess(),time.frameTime);
+    const pauseBtn = document.querySelector(".pause-btn-header");
+    if (pauseBtn) pauseBtn.addEventListener("click", () => pauseGame());
 
-    const main = document.querySelector('.main');
-    if(!shotListenerFlag)main.addEventListener('click', (event) =>shot(event));
-    shotListenerFlag=true;
-    
+    time.moveIntervalId = setInterval(() => gameProcess(), time.frameTime);
+
+    const main = document.querySelector(".main");
+    if (!shotListenerFlag) main.addEventListener("click", (event) => shot(event));
+    shotListenerFlag = true;
 }
-
-
