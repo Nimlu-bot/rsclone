9bd23052acccb8ef74f7dcf4612d3b94e57ca863
fiodar miculich <feotheodor@gmail.com>
2021-01-25 20:24:27 +0300

    feat: I added toggle for theme and fixed login

--- src/components/first-pages/psges-list/settings/settings-pages.template.js
+++ src/components/first-pages/psges-list/settings/settings-pages.template.js
@@ -3,23 +3,26 @@ import "./settings-pages.component.scss";
 
 export const settingsPagesTemplate = `
 <div class="settings-wrap">
-    <ul>
-        <li>
-            <div class="audio-container">
-                <div class="volume-container">
-                    <img class="audio-img" src="../../../../assets/img/sound.png">
-                    <input type="range" id="volume" min="0" max="2" value="1" step="0.01" class="input-slider">
-                </div>
+    <div class="audio-container">
+        <div class="volume-container">
+            <img class="audio-img" src="../../../../assets/img/sound.png">
+            <input type="range" id="volume" min="0" max="2" value="1" step="0.01" class="input-slider">
+        </div>
 
-                <div class="panner-container">
-                    <span>L</span>
-                    <input type="range" id="panner" min="-1" max="1" value="0" step="0.01" class="input-slider">
-                    <span>R</span>
-                </div>
-            </div>
-				</li>
-				<li class="lang-container"><p class="settings-lang">EN</p><p class="settings-lang">RU</p><p class="settings-lang">BY</p></li>
-        <li class="settings-btn game-btn" id="settings-back">back</li>
-    </ul>
+        <div class="panner-container">
+            <span>L</span>
+            <input type="range" id="panner" min="-1" max="1" value="0" step="0.01" class="input-slider">
+            <span>R</span>
+        </div>
+    </div>
+
+    <div class="lang-container"><p class="settings-lang">EN</p><p class="settings-lang">RU</p><p class="settings-lang">BY</p></div>
+    <div class="theme-wrapp">
+        <input type="radio" name="input_theme" value="0">
+        <input type="radio" name="input_theme" value="1">
+        <input type="radio" name="input_theme" value="2">
+        <input type="radio" name="input_theme" value="3">
+    </div>
+    <div class="settings-btn game-btn" id="settings-back">back</div>
 </div>
 `;
